69ba795507718acd57558c779a8a777465a0723e
unscript <alexkh34@gmail.com>
2017-02-06 21:06:15 +0300

    add about

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-//var users = require('./routes/users');
+var about = require('./routes/about');
 
 var app = express();
 
@@ -23,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use("/about", about)
 //app.use('/users', users);
 
 // catch 404 and forward to error handler

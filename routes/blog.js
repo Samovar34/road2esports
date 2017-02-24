baa35fb7f9997ef15879c928aa21756669ebb0e4
unscript <alexkh34@gmail.com>
2017-02-24 23:38:59 +0300

    fix path to read posts

--- routes/blog.js
+++ routes/blog.js
@@ -3,39 +3,47 @@ const router = express.Router();
 const path = require("path");
 const fs = require("fs");
 
+const pug = require("pug");
+
 // TODO кеширование запросов, которые завершились успешно
 let cache ={};
 
-/* GET home page. */
+/* GET blog page. */
 router.get('/', function(req, res, next) {
   // отобразить все записи с пейджингом
   res.render('blog', { curUrl: "/blog" });
   res.end("blog");
 });
 
-
-
-/* GET home page. */
-router.get('/:name', (req, res, next) =>     {
+// read post
+router.get('/read/:name', (req, res, next) =>     {
     if (cache[req.params.name]) {
-        res.render("posts/" + req.params.name, {
-          curUrl: "/blog"
-        });  
+        renderPost(res, req.params.name); 
     } else {
-        if (fs.access("views/posts/" + req.params.name + ".pug", fs.constants.R_OK, (err) => {
+        fs.access("views/posts/" + req.params.name + ".pug", fs.constants.R_OK, (err) => {
             if (err) {
+                // если файла не существует то 404
                 if (err.code === "ENOENT") {
-                    err.status = 400;
+                    err.status = 404;
                 }
                 next(err);
             } else {
                 cache[req.params.name] = true;
-                res.render("posts/" + req.params.name, {
-                  curUrl: "/blog"
-                });
+                renderPost(res, req.params.name); 
             }
-        }));
+        });
     }
 });
+
+/*
+ * ответ запросу клиент
+ * @param {object} res - объект ответа express
+ * @param {string} name - имя шаблона
+ */
+function renderPost(res, name) {
+    res.render("posts/" + name, {
+        curUrl: "/blog"
+    });  
+}
 
 module.exports = router;
